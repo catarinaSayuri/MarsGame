4e8151887b7b0be828d4a4bc9d1a21fdd8e3ccda
Catarina Sayuri <catarina.arashiro@gmail.com>
2025-02-13 14:45:38 -0300

    codigo comentado

--- src/scenes/SceneInicial.js
+++ src/scenes/SceneInicial.js
@@ -1,36 +1,47 @@
 export default class SceneInicial extends Phaser.Scene {
   constructor() {
+    // Chama o construtor da classe Phaser.Scene e define o nome da cena como "SceneInicial"
     super("SceneInicial");
   }
 
   preload() {
+    // Carrega as imagens que serão usadas na cena
     this.load.image("botao", "assets/imagens/botao.png");
     this.load.image("fundo", "assets/imagens/imagesb (2).png");
     this.load.image("logo", "assets/imagens/logo.png");
+
+    // Exibe uma mensagem no console para indicar que os assets foram carregados
     console.log("SceneInicial: Assets carregados.");
   }
 
   create() {
+    // Adiciona a imagem de fundo na posição (350, 300) e reduz seu tamanho
     this.add.image(350, 300, "fundo").setScale(0.6);
+
+    // Adiciona a logo na posição (350, 80) e reduz seu tamanho
     this.add.image(350, 80, "logo").setScale(0.4);
 
+    // Adiciona o botão na posição (350, 500) e reduz seu tamanho
     let botao = this.add.image(350, 500, "botao").setScale(0.5);
+    
+    // Torna o botão interativo para capturar eventos do mouse
     botao.setInteractive();
 
+    // Evento quando o mouse passa sobre o botão
     botao.on("pointerover", () => {
       console.log("Mouse sobre o botão");
     });
 
+    // Evento quando o mouse sai do botão
     botao.on("pointerout", () => {
       console.log("Mouse saiu do botão");
     });
 
+    // Evento quando o botão é clicado
     botao.on("pointerdown", () => {
       console.log("Botão clicado! Carregando cena do jogo...");
+      // Troca para a cena do jogo
       this.scene.start("SceneJogo");
     });
   }
-
-
-  
 }
--- src/scenes/SceneJogo.js
+++ src/scenes/SceneJogo.js
@@ -1,10 +1,14 @@
 export default class SceneJogo extends Phaser.Scene {
   constructor() {
+    // Chama o construtor da classe Phaser.Scene e define o nome da cena como "SceneJogo"
     super("SceneJogo");
   }
 
   preload() {
+    // Carrega a imagem do fundo do mapa
     this.load.image("background", "assets/imagens/mapab.png");
+
+    // Carrega a spritesheet e o arquivo JSON contendo os frames de animação do personagem
     this.load.atlas(
       "boneco",
       "assets/sprites/spritesheet.png",
@@ -13,14 +17,17 @@ export default class SceneJogo extends Phaser.Scene {
   }
 
   create() {
+    // Adiciona o fundo da cena na posição (325, 320) e ajusta o tamanho
     this.add.image(325, 320, "background").setScale(0.8);
 
+    // Cria as teclas de movimentação usando W, A, S, D e as setas do teclado
     this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     this.cursors = this.input.keyboard.createCursorKeys();
 
+    // Cria a animação para andar para frente
     this.anims.create({
       key: "frente",
       frames: this.anims.generateFrameNames("boneco", {
@@ -30,9 +37,10 @@ export default class SceneJogo extends Phaser.Scene {
         zeroPad: 4,
       }),
       frameRate: 10,
-      repeat: -1,
+      repeat: -1, // Faz a animação rodar em loop
     });
 
+    // Cria a animação para andar para trás
     this.anims.create({
       key: "costas",
       frames: this.anims.generateFrameNames("boneco", {
@@ -45,6 +53,7 @@ export default class SceneJogo extends Phaser.Scene {
       repeat: -1,
     });
 
+    // Cria a animação para andar para a direita
     this.anims.create({
       key: "direita",
       frames: this.anims.generateFrameNames("boneco", {
@@ -57,6 +66,7 @@ export default class SceneJogo extends Phaser.Scene {
       repeat: -1,
     });
 
+    // Cria a animação para andar para a esquerda
     this.anims.create({
       key: "esquerda",
       frames: this.anims.generateFrameNames("boneco", {
@@ -69,27 +79,38 @@ export default class SceneJogo extends Phaser.Scene {
       repeat: -1,
     });
 
+    // Adiciona o personagem na posição (250, 200), ajusta o tamanho e define colisão com os limites da tela
     this.boneco = this.physics.add.sprite(250, 200, "boneco").setScale(2);
-    this.boneco.setCollideWorldBounds(true);
-    this.boneco.setOrigin(0.5, 0.5);
+    this.boneco.setCollideWorldBounds(true); // Impede que o personagem saia da tela
+    this.boneco.setOrigin(0.5, 0.5); // Define a origem do sprite no centro
   }
 
   update() {
+    // Reseta a velocidade do personagem antes de verificar os inputs
     this.boneco.setVelocity(0);
 
+    // Movimento para a esquerda (tecla A ou seta esquerda)
     if (this.cursors.left.isDown || this.a.isDown) {
-      this.boneco.setVelocityX(-160);
-      this.boneco.anims.play("esquerda", true);
-    } else if (this.cursors.right.isDown || this.d.isDown) {
-      this.boneco.setVelocityX(160);
+      this.boneco.setVelocityX(-160); // Move o personagem para a esquerda
+      this.boneco.anims.play("esquerda", true); // Ativa a animação correspondente
+    } 
+    // Movimento para a direita (tecla D ou seta direita)
+    else if (this.cursors.right.isDown || this.d.isDown) {
+      this.boneco.setVelocityX(160); // Move o personagem para a direita
       this.boneco.anims.play("direita", true);
-    } else if (this.cursors.up.isDown || this.w.isDown) {
-      this.boneco.setVelocityY(-160);
+    } 
+    // Movimento para cima (tecla W ou seta para cima)
+    else if (this.cursors.up.isDown || this.w.isDown) {
+      this.boneco.setVelocityY(-160); // Move o personagem para cima
       this.boneco.anims.play("costas", true);
-    } else if (this.cursors.down.isDown || this.s.isDown) {
-      this.boneco.setVelocityY(160);
+    } 
+    // Movimento para baixo (tecla S ou seta para baixo)
+    else if (this.cursors.down.isDown || this.s.isDown) {
+      this.boneco.setVelocityY(160); // Move o personagem para baixo
       this.boneco.anims.play("frente", true);
-    } else {
+    } 
+    // Caso nenhuma tecla seja pressionada, para o personagem e interrompe a animação
+    else {
       this.boneco.setVelocity(0);
       this.boneco.anims.stop();
     }
